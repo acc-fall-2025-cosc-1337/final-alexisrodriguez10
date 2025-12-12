d56a892a930cd0666a006eb8b914d2dce0a9aca5
alexisrodriguez10 <alexis.rodriguez10@g.austincc.edu>
2025-12-12 05:55:17 +0000

    Question 1

--- src/die.cpp
+++ src/die.cpp
@@ -1,1 +1,11 @@
-//
+#include "die.h"
+#include <random>
+
+int Die::roll() 
+{
+    static std::random_device rd;
+    static std::mt19937 engine(rd());
+    std::uniform_int_distribution<int> dist(1, sides);
+
+    return dist(engine);
+}
